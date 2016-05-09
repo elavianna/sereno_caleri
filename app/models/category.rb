31197b348514792ce016a9399f613b804d2c876c
elavianna <elasvianna@gmail.com>
2016-05-09 20:46:12 -0300

    adicionando o campo category a view de items + category_id to parameters

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-has_many :item
+has_many :item, dependent: :destroy
 end
---
+++ db/migrate/20160509232646_add_column_category_id_to_item.rb
@@ -0,0 +1,5 @@
+class AddColumnCategoryIdToItem < ActiveRecord::Migration
+  def change
+add_column :items, :category_id, :integer
+  end
+end
