7f1b3bb6e9ed6306e6c0818713bc06b1e566afc4
elavianna <elasvianna@gmail.com>
2016-05-09 21:04:13 -0300

    acho que tô fazendo m

--- db/migrate/20160509231031_drop_add_paperclip_to_iem.rb
+++ db/migrate/20160509231031_drop_add_paperclip_to_iem.rb
@@ -1,5 +1,8 @@
 class DropAddPaperclipToIem < ActiveRecord::Migration
   def change
+#drop_table :addpapercliptoitem
 
   end
 end
+
+#Tava na internet que Drop, em teoria, removia a AddPaperclipToIem, mas nao funcionou, nao consegui apagar mais nada e ficou tarde e fiquei cansada e deixei assim mesmo! hihi
