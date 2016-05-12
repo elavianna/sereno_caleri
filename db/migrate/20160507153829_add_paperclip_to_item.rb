b28bae8d8ccdb15b8f0158fa902ba865e1f547ee
elavianna <elasvianna@gmail.com>
2016-05-12 01:11:27 -0300

    adicionei o git ignore

--- db/migrate/20160507153829_add_paperclip_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddPaperclipToItem < ActiveRecord::Migration
-  def change
-
-  end
-end
--- db/migrate/20160509231031_drop_add_paperclip_to_iem.rb
+++
@@ -1,8 +0,0 @@
-class DropAddPaperclipToIem < ActiveRecord::Migration
-  def change
-#drop_table :addpapercliptoitem
-
-  end
-end
-
-#Tava na internet que Drop, em teoria, removia a AddPaperclipToIem, mas nao funcionou, nao consegui apagar mais nada e ficou tarde e fiquei cansada e deixei assim mesmo! hihi
