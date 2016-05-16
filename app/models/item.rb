e16c04593122136986975fb0d31959a68d331fcf
elavianna <elasvianna@gmail.com>
2016-05-16 20:19:19 -0300

    Nem sei mais o que eu mudei, mas nao funcionou!

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  has_attached_file :image, styles: { small: "64x64#", med: "100x100>", large: "200x200>" }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }, :presence => true,  :size => { :in => 0..1000.kilobytes }
 end
